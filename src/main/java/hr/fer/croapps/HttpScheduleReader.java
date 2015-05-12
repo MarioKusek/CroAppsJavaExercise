b65c8faa1f4d0942bac28f49db56a1444a36564c
mkusek <mario.kusek@fer.hr>
2015-05-12 11:39:38 +0200

    Downloading from URL with URLConnection

--- src/main/java/hr/fer/croapps/HttpScheduleReader.java
+++ src/main/java/hr/fer/croapps/HttpScheduleReader.java
@@ -1,5 +1,10 @@
 package hr.fer.croapps;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,7 +19,28 @@ public class HttpScheduleReader {
 
     // fetch data from net and populate list
     public void fetchSchedule() {
+        String content = downloadContent();
+        System.out.println(content);
+    }
 
+    private String downloadContent() {
+        try {
+            URL url = new URL(scheduleUrl);
+            URLConnection connection = url.openConnection();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line = in.readLine();
+            StringWriter sw = new StringWriter();
+            while (line != null) {
+                sw.write(line);
+                sw.write("\n");
+                line = in.readLine();
+            }
+            return sw.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Can not download schedule!", e);
+        }
     }
 
     public List<ScheduleEntry> getList() {
